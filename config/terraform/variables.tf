ab49f94a2674e71257061e14270b5d6e522e8f32
Du Yuyang <du.yuyang@auspost.com.au>
2016-10-22 15:10:15 +1100

    add basic terraform config

---
+++ config/terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "aws_access_key" {
+  description = "AWS access key."
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key."
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+  default = "duy-demo"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "ap-southeast-2"
+}
+
+# Ubuntu 14.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    ap-southeast-2 = "ami-ba3e14d9"
+  }
+}
