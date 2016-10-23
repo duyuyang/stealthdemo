836c92971ff287c1239d17689e8fdec218812973
Du Yuyang <du.yuyang@auspost.com.au>
2016-10-23 18:22:04 +1100

    fix state file

--- stealthdemo/inventory.py
+++ stealthdemo/inventory.py
@@ -59,5 +59,5 @@ class InventoryGenerator(object):
 
 if __name__ == '__main__':
     IG = InventoryGenerator('config/ansible/hosts',
-                            'terraform.tfstate.backup')
+                            'terraform.tfstate')
     IG.main()
