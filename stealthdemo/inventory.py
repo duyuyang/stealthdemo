7c8107e2cc74bf0ea4bb0c77c193ab5149d65dd4
Du Yuyang <du.yuyang@auspost.com.au>
2016-10-23 18:05:36 +1100

    allow multiple app servers

--- stealthdemo/inventory.py
+++ stealthdemo/inventory.py
@@ -12,16 +12,18 @@ class InventoryGenerator(object):
             data = json.load(data_file)
         return data
 
-    def _writeInventory(self, ip):
+    def _writeInventory(self, ip_list):
         l = []
         l.append('[all_servers:vars]')
-        l.append('ansible_ssh_private_key_file = /home/ubuntu/.ssh/duy-demo.pem')
+        l.append('ansible_ssh_private_key_file =\
+                 /home/ubuntu/.ssh/duy-demo.pem')
         l.append('')
         l.append('[all_servers:children]')
         l.append('wordpress')
         l.append('')
         l.append('[wordpress]')
-        l.append(ip)
+        for ip in ip_list:
+            l.append(ip)
         data = '\n'.join(l)
         with open(self.host, 'w') as data_file:
             data_file.write(data)
@@ -32,14 +34,30 @@ class InventoryGenerator(object):
 
     def _get_instance_private_ip(self):
         resources = self._get_resource()
-        return resources['aws_instance.web']['primary']['attributes']['private_ip']
+        try:
+            """ 1 server behind elb"""
+            return [resources['aws_instance.web']\
+                    ['primary']['attributes']['private_ip']]
+        except KeyError:
+            """ multiple servers behind elb"""
+            ip = []
+            index = 0
+            while  True:
+                try:
+                    key = "aws_instance.web.%s" % index
+                    ip.append(resources[key]['primary']\
+                              ['attributes']['private_ip'])
+                    index = index + 1
+                except KeyError:
+                    return ip
 
 
     def main(self):
-        private_ip = self._get_instance_private_ip()
-        self._writeInventory(private_ip)
+        private_ip_list = self._get_instance_private_ip()
+        self._writeInventory(private_ip_list)
 
 
 if __name__ == '__main__':
-    IG = InventoryGenerator('config/ansible/hosts', 'terraform.tfstate')
+    IG = InventoryGenerator('config/ansible/hosts',
+                            'terraform.tfstate.backup')
     IG.main()
